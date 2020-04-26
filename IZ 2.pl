a6449e28f0bbb0e99b2bcf0cf1cee248ef208e26
Andrew <andrey.pri@mail.ru>
2020-04-26 11:32:32 +0300

    IZ 2 is 99% done

--- IZ 2.pl
+++ IZ 2.pl
@@ -1,8 +1,9 @@
-build_list:-read_list(List1), read_list(List2), filter(List1, ResList1), filter(List2, ResList2), main(ResList1, ResList2, Result1), main(ResList2, ResList1, Result2), append(Result1, Result2, List), write(List).
+build_list:- write('Количество элементов первого списка: '),read(N1), write('Введите элементы первого списка'), read_list(List1, N1), write('Количество элементов второго списка: '), read(N2), write('Введите элементы второго списка'), read_list(List2, N2), filter(List1, ResList1), filter(List2, ResList2), main(ResList1, ResList2, Result1), main(ResList2, ResList1, Result2), append(Result1, Result2, List), write(List).
 
-read_list(A, N):- read_list([], A, 0, N).
-read_list(A, A, N, N):- !.
-read_list(List, A, I, N):- I1 is I+1, read(X), append(List, [X], List1), read_list(List1, A, I1, N).
+read_list(A,N):-read_list([],A,0,N).
+read_list(A,A,N,N):-!.
+read_list(List,A,I,N):-	I1 is I+1,read(X),append(List,[X],List1),
+						read_list(List1,A,I1,N).
 
 main(List1, List2, ResultList):- main(List1, List2, [], ResultList).
 main([], _, CurList, CurList):-!.
