39cb6de21fb0103ea0936d6e55a3c51a21b2f3fd
Andrew <andrey.pri@mail.ru>
2020-06-01 13:08:39 +0300

    IZ 5 still in process

--- IZ 5.pl
+++ IZ 5.pl
@@ -1,4 +1,4 @@
-iz5:- write('Введите n: '), read(N), write('Введите k: '), read(K), tell('file.txt'), choose_letters1([a,b,c,d,e,f], N, K).
+iz5:- write('Введите n: '), read(N), write('Введите k: '), read(K), tell('file.txt'), trace, choose_letters1([a,b,c,d,e,f], N, K).
 iz5:- told.
 
 /*Выбор трех букв, которые будут повторятся по 2 раза*/
@@ -17,12 +17,15 @@ sochet([],0,_):-!.
 sochet([H|Sub_set],K,[H|Set]):-K1 is K-1,sochet(Sub_set,K1,Set).
 sochet(Sub_set,K,[_|Set]):-sochet(Sub_set,K,Set).
 
+/*Построение перестановок с повторениями из выбранных букв*/
 perm_p(Word,Letters2,LetterK,K):- perm_p1(Letters2,0,LetterK,K,Word).
 
-perm_p1([],_,LetterK,K,Word):- perm_p2(LetterK,0,K,Word).
-perm_p1([_|T],2,LetterK,K,Word):- perm_p1(T,0,LetterK,K,Word).
-perm_p1([H|_],Counter,LetterK,K,CurWord):- insert_rep_let(H, CurList, CurList1), NewCounter2 is Counter2 + 1, perm_p(_, N1, N1, Char2, NewCounter2, N2, CurList1), !.
-perm_p(Char1, Counter1, N1, Char2, 0, N2, CurList):- insert_rep_let(Char1, CurList, CurList1), NewCounter1 is Counter1 + 1, perm_p(Char1, NewCounter1, N1, Char2, 0, N2, CurList1).
+perm_p1([],_,[LetterK|_],K,Word):- perm_p2(LetterK,0,K,Word).
+perm_p1([_|T],1,LetterK,K,Word):- perm_p1(T,0,LetterK,K,Word).
+perm_p1([H|T],Counter,LetterK,K,CurWord):- insert_rep_let(H, CurWord, CurWord1), Counter1 is Counter + 1, perm_p1([H|T],Counter1,LetterK,K,CurWord1),!.
+
+perm_p2(_,K,K,Word):- write_str(Word), write("\n"), !, fail.
+perm_p2(LetterK,Counter,K,Word):- insert_rep_let(LetterK,Word,Word1), Counter1 is Counter+1, perm_p2(LetterK,Counter1,K,Word1).
 
 insert_rep_let(El, [El|T], [El|Result]):- insert_rep_let(El, T, Result), !.
 insert_rep_let(El, List, [El|List]).
@@ -30,7 +33,6 @@ insert_rep_let(El, [H|T], [H|Result]):- insert_rep_let(El, T, Result).
 
 in_list_exlude([El|T],El,T).
 in_list_exlude([H|T],El,[H|Tail]):-in_list_exlude(T,El,Tail).
-
 
 write_str([]):-!.
 write_str([H|T]):-put(H),write_str(T).
