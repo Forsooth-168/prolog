108b7caaf2811f5a8cd2c4501fb3d21d2aabde03
Andrew <andrey.pri@mail.ru>
2020-06-03 08:27:51 +0300

    IZ 7 is 10% done

---
+++ IZ 7.pl
@@ -0,0 +1,3 @@
+:-dynamic country/2, form/2, color/2, whenFounded/2, porous/2.
+
+akinator:- readChocolatesFromFile, q1(A1), q2(A2), q3(A3), q4(A4), q5(A5), determineChocolate(A1, A2, A3, A4, A5), removeFacts.
